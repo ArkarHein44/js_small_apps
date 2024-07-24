651456989578553f8d0b66c061479548df57c825
Arkar Hein <arkerhein44@gmail.com>
2024-07-24 22:52:18 +0630

    24.7.2024 overtime lessons

--- l4slider/app.js
+++ l4slider/app.js
@@ -1,2 +1,16 @@
 //UI
-const getdownloadbtn = document.querySelector('.download-btn');
+const slides = document.getElementByClassName('carousel-item');
+const dots = document.querySelectorAll('.dot');
+//console.log(slides);
+//console.log(dots);
+
+let currslide = 1;
+
+document.getElementById('prev').addEventListener('click',function(){
+    //console.log('i am prev');
+})
+
+document.getElementById('next').addEventListener('click',function(){
+    //console.log('i am next');
+})
+
