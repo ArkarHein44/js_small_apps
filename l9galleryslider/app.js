f7824eb354d66804ccfdd974f9cbeb32b3dc99cb
Arkar Hein <arkerhein44@gmail.com>
2024-09-06 21:15:18 +0630

    6.9.2024 lessons

--- l9galleryslider/app.js
+++ l9galleryslider/app.js
@@ -1,1 +1,41 @@
 //UI
+const getmodal = document.querySelector('.modal');
+const getmodalimg = document.querySelector('.modal-img');
+const getcaption = document.querySelector('.caption');
+const getbtnclose = document.querySelector('.btn-close');
+
+
+for(var x=0; x < getimgs.length; x++){
+	getimgs[x].addEventListener("click",function(){
+		shownow(this);
+	});
+}
+
+
+function shownow(ele){
+	// console.log(ele);
+    // console.log(ele.target);
+	// console.log(ele.target.src);
+	// console.log(ele.target.alt);
+
+	getmodal.style.display = "block";
+	getmodalimg.src = ele.target.src;
+	getcaption.textContent = ele.target.alt; 
+}
+
+// getbtnclose.addEventListener("click",function(){
+// 	getmodal.style.display = "none";
+// });
+
+getbtnclose.onclick = function(){
+	getmodal.style.display = "none";
+}
+
+
+document.addEventListener("click",function(e){
+	// console.log(e.target);
+
+	if(e.target === getmodal){
+		getmodal.style.display = "none";
+	}
+}) ;
